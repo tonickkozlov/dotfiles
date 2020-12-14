07b7f87dd52617693f5149df0d304326d1216124
Anton Kozlov <antonk@lyft.com>
2020-12-14 12:22:45 +0000

    Add FZF installed by VIM

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
 # disable greeting
 set fish_greeting
+set -x PATH "$PATH" ~/.vim/plugged/fzf/bin
+source ~/.vim/plugged/fzf/shell/key-bindings.fish
 fzf_key_bindings
 alias git_branch "git rev-parse --abbrev-ref HEAD"
