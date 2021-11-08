5ede8c7092187ce385c4f4194e6ee547173a9cd4
Anton Kozlov <anton.kozlov@woven-planet.global>
2021-11-08 16:59:29 +0000

    set EDITOR

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 # disable greeting
 set fish_greeting
-set --append PATH $HOME/.vim/plugged/fzf
+set --append PATH $HOME/.vim/plugged/fzf/bin
+set -x EDITOR (which vi)
 source ~/.vim/plugged/fzf/shell/key-bindings.fish
 fzf_key_bindings
 alias git_branch "git rev-parse --abbrev-ref HEAD"
