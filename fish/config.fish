26ea7c921de02eff39d5fded30028de79ee53544
Anton Kozlov <anton@cloudflare.com>
2022-04-22 11:20:06 +0100

    [git] remove parent-branch

--- fish/config.fish
+++ fish/config.fish
@@ -6,4 +6,3 @@ set -x EDITOR (which vi)
 source ~/.vim/plugged/fzf/shell/key-bindings.fish
 fzf_key_bindings
 alias git_branch "git rev-parse --abbrev-ref HEAD"
-alias git_parent_branch "git show-branch | grep '*' | grep -v (git rev-parse --abbrev-ref HEAD) | head -n1 | sed 's/.*\[\(.*\)\].*/\1/' | sed 's/[\^~].*//'"
