0748a76725eb279ff349dfdd76f15f7ccde73fe6
Anton Kozlov <anton@cloudflare.com>
2022-08-05 14:49:01 +0100

    [fish] 256 colors

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,8 @@ set fish_greeting
 set --append PATH "$HOME/.vim/plugged/fzf/bin"
 set --prepend PATH "$HOME/bin"
 set -x EDITOR (which vi)
+set -x TERM xterm-256color
+
 source ~/.vim/plugged/fzf/shell/key-bindings.fish
 fzf_key_bindings
 alias git_branch "git rev-parse --abbrev-ref HEAD"
