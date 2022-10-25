60415bd116d9a8945ec1280da0362051cb0a9643
Anton Kozlov <anton@cloudflare.com>
2022-10-25 17:50:36 +0100

    [fish] Add '@@@' to prompt for searching

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -13,7 +13,7 @@ function fish_prompt --description 'Informative prompt'
         set -l statusb_color (set_color --bold $fish_color_status)
         set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-        printf '[%s] %s%s@%s %s%s %s%s%s> ' (date "+%H:%M:%S") (set_color brblue) \
+        printf '[%s] %s%s@%s %s%s %s%s%s@@@ > ' (date "+%H:%M:%S") (set_color brblue) \
             $USER (prompt_hostname) (fish_vcs_prompt) (set_color $fish_color_cwd) $PWD $pipestatus_string \
             (set_color normal)\n
     end
